5aafe4a9fffca8685fb946d506856198fb9cdfc1
dawid27xx <dawidstepien27@gmail.com>
2026-01-09 10:46:23 +0000

    functional chroma + agent template

---
+++ src/agent/agent.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from langchain.agent import create_agent
+from langchain_openai import ChatOpenAI
+import tools
+
+load_dotenv()
+os.environ["API_KEY"] = os.getenv("API_KEY")
+
+def init_agent():
+    tool1 = tools.get_value
+    tool2 = tools.search_documents
+
+    model = ChatOpenAI(
+        model="gpt-4o",
+        temperature=0.1,
+        max_tokens=1000,
+        timout=30
+    )
+
+    agent = create_agent(
+        model=model,
+        tools = [tool1, tool2],
+        system_prompt="System prompt",
+    )
+    
+    return agent
---
+++ src/agent/tools.py
@@ -0,0 +1,36 @@
+import chromadb
+from langchain.tools import tool
+
+def connect_to_chroma():
+    try:
+        client = chromadb.HttpClient(host="localhost", port=8000)
+        collection = client.get_collection(name="docs")
+        return collection
+    except Exception as e:
+        return None
+    
+@tool
+def get_value(input: str) -> int:
+    """get a value based on an input"""
+    return 1
+
+@tool
+def search_documents(query: str) -> str:
+    """Search documents for relevant information and return the top 3 results"""
+    
+    collection = connect_to_chroma()
+    
+    if collection:
+        results = collection.query(
+            query_texts=[query],
+            n_results=3
+        )
+        
+        documents = results.get("documents")
+        if not documents or not documents[0]:
+            return "No relevant information was found"
+        
+        return "\n".join(documents[0])
+    
+    else: return "Could not connect to Chroma"
+    

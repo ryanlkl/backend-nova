06d9491369a7e8bdcd349f02b5e546a68f7242f9
ryanlkl <ryanla94@gmail.com>
2026-01-12 11:21:41 +0000

    Added routers and schema dir

--- src/routers/payment.py
+++ src/routers/payment.py
@@ -1,3 +1,54 @@
 """
 Docstring for routers.payment
 """
+from fastapi import APIRouter
+
+payment_router = APIRouter(prefix="/payment")
+
+## CRUD Endpoints for Payment
+@payment_router.get("/")
+async def list_payments():
+    """
+    Docstring for list_payments
+    """
+    return {"message": "List of payments"}
+
+@payment_router.get("/{payment_id}")
+async def get_payment(payment_id: str):
+    """
+    Docstring for get_payment
+    
+    :type payment_id: str
+    """
+    return {"message": f"Details of payment {payment_id}"}
+
+@payment_router.post("/") # Include appropriate schema
+async def create_payment():
+    """
+    Docstring for create_payment
+    """
+    return {"message": "Payment created"}
+
+@payment_router.put("/{payment_id}") # Include appropriate schema
+async def update_payment(payment_data: dict, payment_id: str):
+    """
+    Docstring for update_payment
+    
+    :param payment_data: Description
+    :type payment_data: dict
+    :param payment_id: Description
+    :type payment_id: str
+    """
+    return {
+        "message": f"Payment {payment_id} updated",
+        "data": payment_data
+        }
+
+@payment_router.delete("/{payment_id}")
+async def delete_payment(payment_id: str):
+    """
+    Docstring for delete_payment
+    
+    :type payment_id: str
+    """
+    return {"message": f"Payment {payment_id} deleted"}

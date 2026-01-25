92f22d9ca930d5547ec4ebe0bc46da5748869077
ryanlkl <ryanla94@gmail.com>
2026-01-25 17:07:44 +0000

    Updated conftest to set test env variables

--- conftest.py
+++ conftest.py
@@ -1,5 +1,15 @@
+"""
+Docstring for conftest
+"""
+import os
 import sys
 from pathlib import Path
 
 project_root = Path(__file__).parent
 sys.path.insert(0, str(project_root))
+
+os.environ.setdefault('DB_PORT', '5432')
+os.environ.setdefault('DB_USER', 'test_user')
+os.environ.setdefault('DB_PASSWORD', 'test_password')
+os.environ.setdefault('DB_HOST', 'localhost')
+os.environ.setdefault('DB_NAME', 'test_db')
